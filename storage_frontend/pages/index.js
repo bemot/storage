82682fd35444f3f88621a0121b23bbe5c1359a51
sasha <sasha@debianmain>
2023-12-28 19:19:22 +0200

    comment

--- storage_frontend/pages/index.js
+++ storage_frontend/pages/index.js
@@ -4,13 +4,26 @@ import Link from 'next/link';
 import { useEffect } from 'react';
 import styles from '../styles/Home.module.css';
 import Heading from '../components/typography/Heading';
+import { useRouter } from 'next/router';
+
+
 export default function Home() {
+
+  const router = useRouter();
+
   const { data: session } = useSession();
 
   useEffect(() => {
     if (session == null) return;
     //console.log('session.jwt', session.jwt);
   }, [session]);
+
+
+  const handleSignOut = async () => {
+    await signOut({ redirect: false });
+    router.push('/auth/sign-in');
+  };
+ 
 
   return (
     <>
@@ -25,22 +38,29 @@ export default function Home() {
           </div>
         )}
         {session ? (
-          <button onClick={signOut}>Sign out</button>
+        //<button onClick={() => signOut({ callbackUrl: `https://engineeringukraine.com.ua/auth/sign-in` })}>Sign out</button>
+          <button onClick={handleSignOut}>Sign out</button>
+
         ) : (
-          <Link href="/auth/sign-in">
-            <button>Sign In</button>
+          <Link href="/auth/sign-in" i passHref>
+            <a>
+              <button>Sign In</button>
+            </a>
           </Link>
         )}
-        <Link href="/protected/">
-          <button
-            style={{
-              marginTop: 10,
-            }}
-          >
-            Protected Page
-          </button>
+        <Link href="/protected/" passHref>
+          <a>
+            <button
+              style={{
+                marginTop: 10,
+              }}
+            >
+              Protected Page
+            </button>
+          </a>
         </Link>
       </div>
     </>
   );
 }
+
